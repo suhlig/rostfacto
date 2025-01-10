11eabdd983ac7016dcd9b54ec81f02e4d74d17a8
Steffen Uhlig <steffen@familie-uhlig.net>
2025-01-10 20:11:50 +0100

    fix: Update integration test selectors and add navigation wait

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -27,13 +27,24 @@ async fn test_retro_workflow() {
     input.send_keys("Test Retro").await.unwrap();
     client.find(fantoccini::Locator::Css("button[type='submit']")).await.unwrap().click().await.unwrap();
     
+    // Click the newly created retro link
+    client.find(fantoccini::Locator::Css("a")).await.unwrap().click().await.unwrap();
+    
+    // Wait a moment for navigation
+    tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+    
     // Test 2: Add items to different columns
-    client.find(fantoccini::Locator::Css(".retro-column.good-column input")).await.unwrap()
-        .send_keys("Good item").await.unwrap();
-    client.find(fantoccini::Locator::Css(".retro-column.bad-column input")).await.unwrap()
-        .send_keys("Bad item").await.unwrap();
-    client.find(fantoccini::Locator::Css(".retro-column.watch-column input")).await.unwrap()
-        .send_keys("Watch item").await.unwrap();
+    let good_input = client.find(fantoccini::Locator::Css("form[hx-post*='/items/Good/'] input")).await.unwrap();
+    good_input.send_keys("Good item").await.unwrap();
+    good_input.send_keys("\n").await.unwrap();
+    
+    let bad_input = client.find(fantoccini::Locator::Css("form[hx-post*='/items/Bad/'] input")).await.unwrap();
+    bad_input.send_keys("Bad item").await.unwrap();
+    bad_input.send_keys("\n").await.unwrap();
+    
+    let watch_input = client.find(fantoccini::Locator::Css("form[hx-post*='/items/Watch/'] input")).await.unwrap();
+    watch_input.send_keys("Watch item").await.unwrap();
+    watch_input.send_keys("\n").await.unwrap();
     
     // Test 3: Toggle item status (Default -> Highlighted -> Completed)
     let good_item = client.find(fantoccini::Locator::Css("#good-items .card")).await.unwrap();
